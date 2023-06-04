7a861d652155035e1a6adf8edb3db6ec3ec7ccda
Omar <oromero@unsa.edu.pe>
2023-06-04 14:34:52 -0500

    Implementado el Ejercicio2c.py las reynas

--- Tarea-del-Ajedrez/Ejercicio2c.py
+++ Tarea-del-Ajedrez/Ejercicio2c.py
@@ -1,2 +1,5 @@
 from interpreter import draw
 from chessPictures import *
+#Hacemos una concatenación usando la función join de la clase picture, 4 veces
+result = queen.join(queen.join(queen.join(queen)))
+draw(result)
