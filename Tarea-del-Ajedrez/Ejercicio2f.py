4bf82fa44ed9b5d223eede322bcd60c18284ce26
Omar <oromero@unsa.edu.pe>
2023-06-04 15:12:46 -0500

    Implementado el Ejercicio2f.py la mitad del tablero

--- Tarea-del-Ajedrez/Ejercicio2f.py
+++ Tarea-del-Ajedrez/Ejercicio2f.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+#Formaremos los cuadrados verticales
+image = Picture(square.negative().img+square.img).verticalRepeat(2)
+#Juntamos la linea image con su negativo, repetimos 4 veces
+result = image.join(image.negative()).horizontalRepeat(4)
+draw(result)
