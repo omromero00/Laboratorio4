d986f81353c24bf37243a1b2668e74c9b64cb866
Omar <oromero@unsa.edu.pe>
2023-06-04 14:40:16 -0500

    Implementado el Ejercicio2d.py una linea del tablero

--- Tarea-del-Ajedrez/Ejercicio2d.py
+++ Tarea-del-Ajedrez/Ejercicio2d.py
@@ -1,2 +1,5 @@
 from interpreter import draw
 from chessPictures import *
+#Juntaremos un cuadrado oscuro con uno claro y repetiremos 4 veces
+result = square.join(square.negative()).horizontalRepeat(4)
+draw(result)
