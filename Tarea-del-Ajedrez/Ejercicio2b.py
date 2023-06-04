de2cac3db9d3946091915e87b37dfa5385bb0db1
Omar <oromero@unsa.edu.pe>
2023-06-04 13:33:36 -0500

    Implementado el Ejercicio2b.py con los caballos de la segunda linea mirando a la derecha

--- Tarea-del-Ajedrez/Ejercicio2b.py
+++ Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,15 @@
 from interpreter import draw
 from chessPictures import *
+#Creo un caballo negativo
+negativeK = knight.negative()
+#Creo la primera linea con el caballo normal y con join agrego el caballo negativo
+firstLine = knight.join(negativeK)
+#Creo el caballo negativo invertido verticalmente
+negativeInv = negativeK.verticalMirror()
+#Creo la segunda linea con el caballo negativamente invertido y con 
+#join agrego el caballo normal invertido verticalmente
+secondLine = negativeInv.join(knight.verticalMirror())
+#Con la función up agrego la primera linea y la segunda linea
+result = firstLine.up(secondLine)
+
+draw(result)
