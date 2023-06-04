6a6b26cf29dbc0a3fde445a507b2697433f2ffb1
Omar <oromero@unsa.edu.pe>
2023-06-04 14:51:12 -0500

    Implementado el Ejercicio2e.py una linea del tablero pero iniciando con oscuro

--- Tarea-del-Ajedrez/Ejercicio2e.py
+++ Tarea-del-Ajedrez/Ejercicio2e.py
@@ -1,2 +1,5 @@
 from interpreter import draw
 from chessPictures import *
+#Una linea del tablero, pero iniciando con oscuro
+result = square.negative().join(square).horizontalRepeat(4)
+draw(result)
